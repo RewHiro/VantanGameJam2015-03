3cff841587670ea4a57554d4921ba61eb4039b45
syougun360 <syougun360@infoseek.jp>
2015-03-11 23:00:00 +0900

    ソウルの数をUI文字として出す。ソウルに吸収されたときのアニメーション実装

---
+++ GameJamProject/Assets/Soul/SoulParameter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulParameter : MonoBehaviour {
+
+    /// <summary>
+    /// 魂の数
+    /// </summary>
+    public long num { get; private set; }
+
+    [SerializeField]
+    SoulTextChanger soulTextChanger = null;
+
+    void Start()
+    {
+        soulTextChanger.Change(0);
+    }
+
+    void Update()
+    {
+        AddNum(1000);
+    }
+
+    /// <summary>
+    /// 数を加算していく
+    /// </summary>
+    /// <param name="value"></param>
+    public void AddNum(long value)
+    {
+        num += value;
+        soulTextChanger.Change(num);
+    }
+
+}

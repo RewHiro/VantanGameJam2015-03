6d8389ea7bc72b29439d34c19f2ced12b6f27fb1
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-03-11 16:50:08 +0900

    マルチ解像度対応、UIは非対応、Itween追加

---
+++ GameJamProject/Assets/Player/PlayerAttacker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetMouseButtonDown(0) || Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+        //    //　弾を生成
+        //}
+	}
+}
---
+++ GameJamProject/Assets/Player/PlayerPalameter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPalameter : MonoBehaviour {
+
+    [SerializeField]
+    int attackPower = 10;
+
+    [SerializeField]
+    int level = 1;
+
+    [SerializeField]
+    int  experience = 0;
+
+    struct AttackPower
+    {
+        int attackPow;
+    }
+
+    enum RANK
+    {
+        BEGINNER,
+        AMOUTURE,
+        PRO,
+        MASTER
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}

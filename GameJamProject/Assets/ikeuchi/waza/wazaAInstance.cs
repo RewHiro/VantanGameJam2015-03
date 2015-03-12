8baa1760459369d111e1a3efd141321c72088351
prinnotamago <ike3shunta@yahoo.co.jp>
2015-03-12 13:25:27 +0900

    必殺技(仮)

---
+++ GameJamProject/Assets/ikeuchi/waza/wazaAInstance.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wazaAInstance : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject Prefab = null;
+	
+	public float Posx = 0.0f;
+	public float Posy = 0.0f;
+
+	public float tamaSize = 0.0f;
+
+	public int counter = 0;
+	public int countTime = 0;
+	public bool onOff = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			Posx = GameObject.Find ("BulletRoot").GetComponent<typeMode> ().Posx;
+			Posy = GameObject.Find ("BulletRoot").GetComponent<typeMode> ().Posy;
+			onOff = true;
+			for (int i = 0; i < 100; i++) {
+				tamaSize = Random.Range (0.5f, 1.0f);
+				//Posx = Random.Range (0.5f, 1.0f);
+				//Posy = Random.Range (0.5f, 1.0f);
+				var clone = (GameObject)Instantiate (Prefab);
+				clone.transform.position = new Vector3 (Posx, Posy, 0.0f);
+				clone.transform.localScale = new Vector3 (tamaSize, tamaSize, tamaSize);
+				clone.transform.SetParent (this.transform);
+			}
+		}
+
+		if (onOff == true) {
+			if(countTime % 10 == 0){
+				for (int i = 0; i < 100; i++) {
+					tamaSize = Random.Range (0.5f, 1.0f);
+					//Posx = Random.Range (0.5f, 1.0f);
+					//Posy = Random.Range (0.5f, 1.0f);
+					var clone = (GameObject)Instantiate (Prefab);
+					clone.transform.position = new Vector3 (Posx, Posy, 0.0f);
+					clone.transform.localScale = new Vector3 (tamaSize, tamaSize, tamaSize);
+					clone.transform.SetParent (this.transform);
+				}
+				counter++;
+			}
+			countTime++;
+			if(counter > 10){
+				onOff = false;
+				counter = 0;
+				countTime = 0;
+			}
+		}
+	}
+}

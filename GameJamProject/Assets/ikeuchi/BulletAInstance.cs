8bd91e946a372622525b6572039c66b8cf95a2a2
prinnotamago <ike3shunta@yahoo.co.jp>
2015-03-12 11:09:52 +0900

    update

--- GameJamProject/Assets/ikeuchi/BulletAInstance.cs
+++ GameJamProject/Assets/ikeuchi/BulletAInstance.cs
@@ -8,25 +8,33 @@ public class BulletAInstance : MonoBehaviour {
 	[SerializeField]
 	private GameObject Prefab = null;
 
-	public float Posx = 0;
-	public float Posy = 0;
-	
+	public float Posx = 0.0f;
+	public float Posy = 0.0f;
+
+	public float exp = 0.0f;
+
 	public int typeShot = 0;
 	
 	// Use this for initialization
 	void Start () {
+		exp = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Z)) {
 			typeShot = GameObject.Find("BulletRoot").GetComponent<typeMode>().type;
-			Posx = GameObject.Find("BulletRoot").GetComponent<typeMode>().Posx;
-			Posy = GameObject.Find("BulletRoot").GetComponent<typeMode>().Posy;
-
 			if (typeShot == 0) {
+				Posx = GameObject.Find("BulletRoot").GetComponent<typeMode>().Posx;
+				Posy = GameObject.Find("BulletRoot").GetComponent<typeMode>().Posy;
+
+				exp += 0.01f;
+
+				//Debug.Log(exp);
+
 				var clone = (GameObject)Instantiate (Prefab);
 				clone.transform.position = new Vector3 (Posx, Posy, 0.0f);
+				clone.transform.localScale = new Vector3 (exp * 0.1f + 0.5f, exp * 0.1f + 0.5f, exp * 0.1f + 0.5f);
 				clone.transform.SetParent (this.transform);
 			}
 		}

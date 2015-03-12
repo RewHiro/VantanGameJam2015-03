4561578add52461e76be2d0c762ab0b9203629a2
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-03-13 00:31:35 +0900

    弾をスプライトに変更しました

--- GameJamProject/Assets/ikeuchi/waza/WazaSortingLayer.cs
+++ GameJamProject/Assets/ikeuchi/waza/WazaSortingLayer.cs
@@ -5,7 +5,6 @@ public class WazaSortingLayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<MeshRenderer> ().sortingLayerName = "waza";
 	}
 	
 	// Update is called once per frame
